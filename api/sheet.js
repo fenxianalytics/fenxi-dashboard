13e2b028d53311b2609ed55e34490926f1227378
fenxianalytics <contact@fenxianalytics.com>
2026-03-01 21:14:22 +0530

    fix correct sheet tab name

--- api/sheet.js
+++ api/sheet.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
 
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SPREADSHEET_ID,
-      range: "Sheet1!A1:D10",
+      range: "Electricity_Statistics_Syntheti!A1:G10",
     });
 
     res.status(200).json(response.data.values);
